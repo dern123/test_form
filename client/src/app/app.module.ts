b7fd3e14b5b3b35ee7dd457aad6259a0eba3e2c3
Denis Stryzhalov <imperor.oktov@gmail.com>
2023-12-05 18:43:32 +0200

    proxy fix

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { HeaderModule } from './components/header/header.module';
 import { TokenInterceptor } from './services/token.interceptor';
 
@@ -25,7 +25,8 @@ import { TokenInterceptor } from './services/token.interceptor';
       multi: true,
       useClass: TokenInterceptor,
     },
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
